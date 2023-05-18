3cd136131f22c80fcaca89d59c5697f5dba584da
Ramiro Roballos <ramiroroballos@gmail.com>
2023-05-18 19:48:01 -0400

    cree rutas iniciales y los enpoints getMotorcycleById y getMotorcycleByname

---
+++ src/controllers/Motorcycle.controller.js
@@ -0,0 +1,48 @@
+const {Item, Motorcycle} = require('../db.js');
+
+
+const getAllMotorcycles = async (req, res) => {
+}
+
+const getMotorcycleById = async (req, res) => {
+
+    const {id} = req.params;
+
+    try {
+        const motorcycle = await Motorcycle.findByPk(id, { include: Item });
+        res.status(200).json(motorcycle);
+
+    } catch (error) {
+        res.status(404).json({error: 'Motorcycle not found'});
+    }
+}
+
+const getMotorcycleByName = async (req, res) => {
+    
+    const {name} = req.query;
+    
+    try {
+        const motorcycle = await Motorcycle.findAll({
+            where: {
+                name: {
+                    [Op.iLike]: `%${name}%`
+                }
+            },
+            include: Item
+        });
+        res.json(motorcycle);
+
+    } catch (error) {
+        res.json({error: 'Motorcycle not found'});
+    }
+}
+
+const createMotorcycles = async (req, res) => {
+}
+
+module.exports = {
+    getMotorcycleById,
+    getAllMotorcycles,
+    createMotorcycles,
+    getMotorcycleByName
+}
