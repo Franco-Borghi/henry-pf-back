18473263dba7dac3f95dd520473eeff7625e9369
franco triadani <francotriadani@gmail.com>
2023-05-17 23:18:38 -0300

    cree el User Model

---
+++ src/models/Users.js
@@ -0,0 +1,52 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    sequelize.define(Users,{
+        id:{
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true
+        },
+        first_name:{
+            type: DataTypes.STRING(50),
+            allowNull: false,
+            validate: {
+                len: {
+                    args: [0, 50],
+                    msg: 'El nombre tiene demasiados carácteres'
+                }
+            }   
+            
+        },
+        last_name:{
+            type: DataTypes.STRING(50),
+            allowNull: false,
+            validate: {
+                len: {
+                    args: [0, 50],
+                    msg: 'El Apellido tiene demasiados carácteres'
+                }
+            }
+        },
+        email:{
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        phone_number:{
+            type: DataTypes.INTIGER,
+            allowNull: false,
+        },
+        password:{
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        active:{
+            type: DataTypes.BOOLEAN,
+        },
+        role:{
+            type: DataTypes.ENUM('admin', 'client'),
+        }
+    })
+     
+};
