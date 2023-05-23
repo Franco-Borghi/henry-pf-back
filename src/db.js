5cd8d6fa968bc878eab783e9bee45c93bd821bf4
Ramiro Roballos <ramiroroballos@gmail.com>
2023-05-22 23:27:37 -0400

    seedDB agregado para que levante la base ya con data siempre

--- src/db.js
+++ src/db.js
@@ -21,22 +21,17 @@ fs.readdirSync(path.join(__dirname, '/models'))
     modelDefiners.push(require(path.join(__dirname, '/models', file)));
   });
 
-// Injectamos la conexion (sequelize) a todos los modelos
+
 modelDefiners.forEach(model => model(sequelize));
-// Capitalizamos los nombres de los modelos ie: product => Product
 let entries = Object.entries(sequelize.models);
 let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
 sequelize.models = Object.fromEntries(capsEntries);
 
-// En sequelize.models están todos los modelos importados como propiedades
-// Para relacionarlos hacemos un destructuring
 const { Motorcycle, Item, Reviews, Orders, Users } = sequelize.models;
 
 /* -------------------------------------------------------------- */
 /* Relations                                                      */
 /* -------------------------------------------------------------- */
-
-//TODO: revisar si es necesario poner foreignKey en ambos o donde seria mejor ponerlo
 
 // Item - Motorcycle
 Motorcycle.hasMany(Item, {
