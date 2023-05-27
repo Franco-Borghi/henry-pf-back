65863666ebb1fb62fda0d5d87ac3b84c428aeb38
LucasSchneebeli <lucasschneebeli@gmail.com>
2023-05-27 15:44:23 -0400

    ruta

--- src/controllers/Users.controller.js
+++ src/controllers/Users.controller.js
@@ -14,6 +14,40 @@ const createUser = async (req, res) => {
     }
 }
 
+const updateUser = async (req, res) => {
+    try{
+        const {firstName, lastName, phoneNumber,} = req.body;
+        const user = await Users.update({ 
+            firstName,
+            lastName,
+            phoneNumber,
+        },
+        { where: { id: req.params.id } }
+
+          )
+        res.status(200).json(user)
+    }catch(err){
+        res.status(400).send(err)
+
+    }
+}
+
+const getUser = async (req, res) => {
+    try{
+        const {id} = req.params;
+        const user = await Users.findOne(
+        { where: { id } }
+
+          )
+        res.status(200).json(user)
+    }catch(err){
+        res.status(400).send(err)
+
+    }
+}
+
 module.exports = {
-    createUser
+    createUser,
+    getUser,
+    updateUser,
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,8 @@ const { Router } = require('express');
 const { getAllMotorcycles, getMotorcycleById, getMotorcycleByName, createMotorcycles } = require('../controllers/Motorcycle.controller');
 const { createOrder } = require('../controllers/Orders.controller');
 const { createUser } = require('../controllers/Users.controller');
+const { getUser, updateUser } = require('../controllers/Users.controller');
+
 
 const router = Router();
 
@@ -16,5 +18,11 @@ router.post('/motorcycles', createMotorcycles)
 router.post('/orders', createOrder)
 
 router.post("/users", createUser)
+
+router.put('/users/:id', updateUser)
+
+router.get('/users/:id', getUser)
+
+
 
 module.exports = router;
