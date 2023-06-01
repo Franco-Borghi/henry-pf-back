2cbd3c12af5df183ccb42e8928060521b609b955
Ramiro Roballos <ramiroroballos@gmail.com>
2023-06-01 15:11:04 -0400

    new endpoint - updateMotorcycles

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const { getAllMotorcycles, getMotorcycleById, getMotorcycleByName, createMotorcycles } = require('../controllers/Motorcycle.controller');
+const { getAllMotorcycles, getMotorcycleById, getMotorcycleByName, createMotorcycles, updateMotorcycle } = require('../controllers/Motorcycle.controller');
 const { createOrder } = require('../controllers/Orders.controller');
 const { createUser } = require('../controllers/Users.controller');
 const { getUser, updateUser } = require('../controllers/Users.controller');
@@ -7,15 +7,31 @@ const { getUser, updateUser } = require('../controllers/Users.controller');
 
 const router = Router();
 
+/* ----------------------------------- */
+/* Motorcycles                         */
+/* ----------------------------------- */
+
 router.get('/motorcycles', (req, res) => {
     if(req.query.name) getMotorcycleByName(req,res)
     else getAllMotorcycles(req,res)
 })
 
 router.get('/motorcycles/:id', getMotorcycleById)
+
+router.put('/motorcycles/:id', updateMotorcycle)
+
 router.post('/motorcycles', createMotorcycles)
 
+
+/* ----------------------------------- */
+/* Orders                              */
+/* ----------------------------------- */
+
 router.post('/orders', createOrder)
+
+/* ----------------------------------- */
+/* Users                               */
+/* ----------------------------------- */
 
 router.post("/users", createUser)
 
