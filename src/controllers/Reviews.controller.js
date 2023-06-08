34ac9ac0ea2cd40994f8e8d7f6df10828cd3d243
Ramiro Roballos <ramiroroballos@gmail.com>
2023-06-08 15:36:35 -0400

    getReviews, getReviewsbyUser y getReviewsByMotorcycle terminados

--- src/controllers/Reviews.controller.js
+++ src/controllers/Reviews.controller.js
@@ -48,11 +48,58 @@ const getReviews = async (req, res) => {
     }
 }
 
+const getReviewsByMotorcycle = async (req, res) => {
+    const { motorcycleId } = req.params;
+    try {
+        const reviews = await Reviews.findAll({
+            where: {
+                motorcycleId
+            },
+            include: [
+                {
+                    model: Users,
+                    attributes: ['firstName', 'lastName','email']
+                },
+                {
+                    model: Motorcycle,
+                    attributes: ['brand', 'model', 'year', 'price']
+                }
+            ]
+        })
+        res.status(200).json(reviews)
+    } catch (err) {
+        res.status(400).json(err)
+    }
+}
 
-
-
+const getReviewsByUser = async (req, res) => {
+    const { userId } = req.params;
+    try {
+        const reviews = await Reviews.findAll({
+            where: {
+                userId
+            },
+            include: [
+                {
+                    model: Users,
+                    attributes: ['firstName', 'lastName','email']
+                },
+                {
+                    model: Motorcycle,
+                    attributes: ['brand', 'model', 'year', 'price']
+                }
+            ]
+        })
+        res.status(200).json(reviews)
+    } catch (err) {
+        res.status(400).json(err)
+    }
+}
 
 module.exports = {
     createReview,
     getReviews,
+    getReviewsByMotorcycle,
+    getReviewsByUser
+
 }
--- src/routes/reviews.routes.js
+++ src/routes/reviews.routes.js
@@ -6,4 +6,7 @@ const reviewsRouter = express.Router();
 reviewsRouter.get("/", getReviews)
 reviewsRouter.post('/', createReview)
 
+reviewsRouter.get("/:motorcycleId", getReviewsByMotorcycle)
+reviewsRouter.get("/:userId", getReviewsByUser)
+
 module.exports = reviewsRouter;
