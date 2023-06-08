b3bbeb3ee2264e3d46c3335549d6a6c0593d3f36
Ramiro Roballos <ramiroroballos@gmail.com>
2023-06-08 15:16:47 -0400

    terminado

--- src/controllers/Reviews.controller.js
+++ src/controllers/Reviews.controller.js
@@ -8,13 +8,11 @@ const createReview = async (req, res) => {
         const user = await Users.findByPk(userId)
         const motorcycle = await Motorcycle.findByPk(motorcycleId)
 
-             // Check whether the user and motorcycle exist
-             if (!user || !motorcycle) {
-                res.status(400).json({message: 'User or Motorcycle does not exist'});
-                return;
-            }
-        console.log("User", user)
-        console.log("motorcycle", motorcycle)
+
+        if (!user || !motorcycle) {
+            res.status(400).json({ message: 'User or Motorcycle does not exist' });
+            return;
+        }
 
         const review = await Reviews.create({
             rating,
@@ -22,12 +20,6 @@ const createReview = async (req, res) => {
             userId: user.id,
             motorcycleId: motorcycle.id
         })
-
-        console.log("Review", review)
- 
-
-        // await review.setUser(user)
-        // await review.setMotorcycle(motorcycle)
 
         res.status(201).json(review)
 
--- src/models/Reviews.js
+++ src/models/Reviews.js
@@ -11,12 +11,10 @@ module.exports = (sequelize) => {
       type: DataTypes.STRING,
       allowNull: true,
     },
-    // Explicitly defining the userId
     userId: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    // Explicitly defining the motorcycleId
     motorcycleId: {
       type: DataTypes.UUID,
       allowNull: false,
